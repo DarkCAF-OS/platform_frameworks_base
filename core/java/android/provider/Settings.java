529a4d41db9fea5e84fcbf51b46329896350b8f2
htchoi1995 <htchoi1995@gmail.com>
2016-01-03 20:26:36 +0100

    FWB: Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3699,6 +3699,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
