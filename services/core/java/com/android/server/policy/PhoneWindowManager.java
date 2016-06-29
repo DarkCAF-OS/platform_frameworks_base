fb441fd60098379ad3f12011bc5302b0b5d61a4b
Carlo Savignano <carlosavignano@aospa.co>
2016-06-29 22:02:08 +0200

    input: Add gestures control
    Includes following commits :
    https://github.com/AOSPA/android_frameworks_base/commit/d8c6bc1680af4f1ae01c1ee468e1e8989b23bb8b
    https://github.com/AOSPA/android_frameworks_base/commit/859bab47ee59957c84aa31e860cdb1d9a2dabc07
    https://github.com/AOSPA/android_frameworks_base/commit/fd9e10091bda162d245d2fd58ca006e488b11cd2
    https://github.com/AOSPA/android_frameworks_base/commit/2eea091f6b0be8f41d886db653bdc71479998420
    https://github.com/AOSPA/android_frameworks_base/commit/325cf5b1756056ca0dffaa2b578ced292fd613bc
    
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -759,6 +759,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
+    // Gesture key handler.
+    private KeyHandler mKeyHandler;
+
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -1903,6 +1906,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mWindowManagerInternal.registerAppTransitionListener(
                 mStatusBarController.getAppTransitionListener());
+
+        boolean enableKeyHandler = context.getResources().
+                getBoolean(com.android.internal.R.bool.config_enableKeyHandler);
+        if (enableKeyHandler) {
+            mKeyHandler = new KeyHandler(mContext);
+        }
     }
 
     private void updateKeyAssignments() {
@@ -5878,6 +5887,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
+        /**
+         * Handle gestures input earlier then anything when screen is off.
+         * @author Carlo Savignano
+         */
+        if (!interactive) {
+            if (mKeyHandler != null && mKeyHandler.handleKeyEvent(event)) {
+                return 0;
+            }
+        }
+
         // Basic policy based on interactive state.
         int result;
         boolean isWakeKey = (policyFlags & WindowManagerPolicy.FLAG_WAKE) != 0
@@ -7228,6 +7247,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         mSystemGestures.systemReady();
         mImmersiveModeConfirmation.systemReady();
+        mKeyHandler.systemReady();
     }
 
     /** {@inheritDoc} */
